06ebb22694e9128478382cc5b68f10630812bf33
Qii <57295427+syauqqii@users.noreply.github.com>
2023-04-28 19:43:10 +0700

    Update start.bat

--- Nomor 3 - Rest API/Automation Project Maker/start.bat
+++ Nomor 3 - Rest API/Automation Project Maker/start.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
 cls
+
+:: -------------------------- [ file processing ] --------------------------
 chdir /d D:\
 mkdir Rest-API
 pushd Rest-API
@@ -17,20 +19,21 @@ echo 	"Rest-API/Controllers">> main.go
 echo )>> main.go
 echo.>> main.go
 echo func main() {>> main.go
-echo 	router := mux.NewRouter()>> main.go
-echo.>> main.go
 echo 	serv := color.New(color.FgCyan, color.Underline)>> main.go
 echo 	port := ":5050">> main.go
 echo.>> main.go
 echo 	log.Print("\n > Server running on: ")>> main.go
-echo 	serv.Printf("http://localhost%%s\n", port)>> main.go
+echo 	serv.Printf("http://localhost%%s\n   ", port)>> main.go
+echo.>> main.go
+echo 	router := mux.NewRouter()>> main.go
 echo.>> main.go
 echo 	router.HandleFunc("/nama", Controllers.CreateData).Methods("POST")>> main.go
 echo 	router.HandleFunc("/semuadata", Controllers.ReadData).Methods("GET")>> main.go
 echo.>> main.go
-echo 	log.Fatal(http.ListenAndServe(port, router)>> main.go
+echo 	log.Fatal(http.ListenAndServe(port, router))>> main.go
 echo }>> main.go
 
+:: -------------------------- [ file processing ] --------------------------
 mkdir Controllers
 pushd Controllers
 type nul > Logic-CRUD.go
@@ -49,7 +52,7 @@ echo 	NIM	string `json:"nim"`>> Logic-CRUD.go
 echo 	Alamat	string `json:"alamat"`>> Logic-CRUD.go
 echo }>> Logic-CRUD.go
 echo.>> Logic-CRUD.go
-echo var datas []data>> Logic-CRUD.go
+echo var datas []Data>> Logic-CRUD.go
 echo.>> Logic-CRUD.go
 echo func CreateData(response http.ResponseWriter, request *http.Request){>> Logic-CRUD.go
 echo 	json.NewEncoder(response).Encode(datas)>> Logic-CRUD.go
@@ -58,15 +61,19 @@ echo.>> Logic-CRUD.go
 echo func ReadData(response http.ResponseWriter, request *http.Request){>> Logic-CRUD.go
 echo 	var data Data>> Logic-CRUD.go
 echo.>> Logic-CRUD.go
-echo 	_ = json.NewDecoder(request.Body).Decode(&data)>> Logic-CRUD.go
+echo 	_ = json.NewDecoder^(request.Body^).Decode^(^&data^)>> Logic-CRUD.go
 echo 	datas = append(datas, data)>> Logic-CRUD.go
 echo.>> Logic-CRUD.go
 echo 	json.NewEncoder(response).Encode(data)>> Logic-CRUD.go
 echo }>> Logic-CRUD.go
 
+
+:: -------------------------- [ init project ] --------------------------
 pushd ..
 go mod init Rest-API
 go get github.com/fatih/color
 go get github.com/gorilla/mux
 go mod tidy
+
+
 pause
