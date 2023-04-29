8cd8680f13b446e62fa386f21dca9a0ff91fa9b5
Qii <57295427+syauqqii@users.noreply.github.com>
2023-04-29 08:21:06 +0700

    Update start.bat

--- Nomor 3 - Rest API/Automation Project Maker/start.bat
+++ Nomor 3 - Rest API/Automation Project Maker/start.bat
@@ -8,31 +8,7 @@ pushd Rest-API
 type nul > main.go
 
 :: ------------------------------ [ main.go ] ------------------------------
-echo package main> main.go
-echo.>> main.go
-echo import (>> main.go
-echo 	"fmt">> main.go
-echo 	"log">> main.go
-echo 	"net/http">> main.go
-echo 	"github.com/fatih/color">> main.go
-echo 	"github.com/gorilla/mux">> main.go
-echo 	"Rest-API/Controllers">> main.go
-echo )>> main.go
-echo.>> main.go
-echo func main() {>> main.go
-echo 	serv := color.New(color.FgCyan, color.Underline)>> main.go
-echo 	port := ":5050">> main.go
-echo.>> main.go
-echo 	fmt.Print("\n > Server running on: ")>> main.go
-echo 	serv.Printf("http://localhost%%s\n", port)>> main.go
-echo.>> main.go
-echo 	router := mux.NewRouter()>> main.go
-echo.>> main.go
-echo 	router.HandleFunc("/nama", Controllers.CreateData).Methods("POST")>> main.go
-echo 	router.HandleFunc("/semuadata", Controllers.ReadData).Methods("GET")>> main.go
-echo.>> main.go
-echo 	log.Fatal(http.ListenAndServe(port, router))>> main.go
-echo }>> main.go
+:: KOSONG - NEXT UPDATE SAJA
 
 :: -------------------------- [ file processing ] --------------------------
 mkdir Controllers
