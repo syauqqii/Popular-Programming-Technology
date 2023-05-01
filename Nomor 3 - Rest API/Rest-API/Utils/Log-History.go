d2eb54a82f88f3c69253ad629e69220ccf98af7b
Qii <57295427+syauqqii@users.noreply.github.com>
2023-05-01 10:18:54 +0700

    Refactor code function Logger()
    Membuat kode lebih efisien

--- Nomor 3 - Rest API/Rest-API/Utils/Log-History.go
+++ Nomor 3 - Rest API/Rest-API/Utils/Log-History.go
@@ -1,29 +1,31 @@
-package Utils
+package main
 
 import (
-	"fmt"
-	"github.com/fatih/color"
-	"time"
+        "github.com/fatih/color"
+	"os"
+        "time"
 )
 
-func Logger(options int, msg string){
-	now := time.Now()	
+func Logger(opt int, msg string) {
+        now := time.Now()
 
-	grn := color.New(color.FgHiGreen)
-	cyn := color.New(color.FgHiCyan)
-	red := color.New(color.FgHiRed)
-	yel := color.New(color.FgHiYellow)
+        white := color.New(color.FgHiWhite); red    := color.New(color.FgHiRed)
+        green := color.New(color.FgHiGreen); yellow := color.New(color.FgHiYellow)
+        cyan  := color.New(color.FgHiCyan);  fatal  := color.New(color.FgRed)
 
-	fmt.Print(" [")
-	yel.Printf("%s", now.Format("15:04:05 2006/01/02"))
-	fmt.Print("] ")
-	if options == 1 {
-		cyn.Print("INFO")
-	} else if options == 2 {
-		red.Print("ERROR")
-	} else if options == 3 {
-		grn.Print("EXIT")
-	}
-	
-	fmt.Printf(": %s\n", msg)
+        white.Printf(" [%s] ", yellow.SprintFunc()(now.Format("15:04:05 2006/01/02")))
+
+        if opt == 1 {
+                cyan.Printf("INFO")
+        } else if opt == 2{
+                red.Printf("ERROR")
+        } else if opt == 3 {
+                green.Printf("SUCCESS")
+        } else if opt == 4 {
+                fatal.Printf("FATAL")
+                white.Println(": "+msg)
+                os.Exit(0)
+        }
+
+        white.Println(": "+msg)
 }
