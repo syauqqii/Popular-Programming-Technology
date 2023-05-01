b9c7a7603e5ecfc4fe24d9d51adfc9a311918e3d
Qii <57295427+syauqqii@users.noreply.github.com>
2023-05-01 12:55:16 +0700

    Update main.go

--- Nomor 3 - Rest API/Rest-API/main.go
+++ Nomor 3 - Rest API/Rest-API/main.go
@@ -1,53 +1,62 @@
+/* Created by Dimas Syauqi Syafa - 2502004405. */
+
+/* Set package program
+ * Untuk memberitaukan bahwa ini program ini dari package main. */
 package main
 
+/* Created by Dimas Syauqi Syafa - 2502004405. */
+
+/* Import Package / Library
+ * Untuk memberikan fitur / fungsi tambahan yang lebih fleksible untuk program ini. */
 import (
-	"fmt"
 	"encoding/json"
-	"github.com/fatih/color"
+	"fmt"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"Rest-API/Controllers"
-	"Rest-API/Utils"
 	"os"
 	"os/signal"
+	"Rest-API/Controllers"
+	"Rest-API/Utils"
 	"syscall"
-	"io/ioutil"
 )
 
+/* Main program
+ * disinilah tempat pemanggilan beberapa function untuk dijalankan pada poin awal / entry poin. */
 func main() {
+	// Panggil function ClearScreen() dari folder Utils/
 	Utils.ClearScreen()
-
-	serv := color.New(color.FgCyan, color.Underline)
+	
+	// Set port untuk menjalankan program berdasarkan port yang telah diset!
 	const port = ":5050"
 
-	fmt.Print("\n > Server running on: ")
-	serv.Printf("http://localhost%s", port)
+	// Menampilkan output variasi, supaya terlihat lebih bagus (output header)
+	fmt.Printf("\n > Server running on: %s", Utils.Serv.Sprintf("http://localhost%s", port))
 	fmt.Println("\n\n -------------------- [ LOG History ] --------------------\n")
 
+	// Set variable router untuk menampung object dari NewRouter
 	router := mux.NewRouter()
 
+	/* Set route program saya sesuai dengan SOAL
+	 * pada /nama menggunakan method POST 		=> berfungsi untuk POST data (Nama NIM Alamat)
+	 * untuk /semuadata menggunakan method GET	=> berfungsi untuk GET data (tampilkan semua data dari array of struct)
+	 *
+	 * /nama akan meneruskan request ke function CreateData() yang berada di folder Controllers
+	 * hasilnya berupa response yang akan di teruskan ke user
+	 *
+	 * /semuadata sama seperti /nama, hanya beda fungsi yaitu menggunakan fungsi ReadData()
+	 */
 	router.HandleFunc("/nama", Controllers.CreateData).Methods("POST")
 	router.HandleFunc("/semuadata", Controllers.ReadData).Methods("GET")
 
-	router.NotFoundHandler = http.HandlerFunc(func(response http.ResponseWriter, request *http.Request){
-		response.Header().Set("Content-Type", "application/json")
-        response.WriteHeader(http.StatusNotFound)
+	/* Untuk handle jika route tidak ditemukan, misal :
+	 * ketika user request root domain / selain '/nama' & '/semuadata'
+	 * maka akan dihandle disini, dengan cara : memanggil function yang ada di folder Controllers/
+	 */
+	router.NotFoundHandler = http.HandlerFunc(Controllers.HandleNotFound)
 
-	    dataL := map[string]string{
-	    	"get": "http://localhost:5050/semuadata",
-	        "post": "http://localhost:5050/nama",
-	    }
-
-	    jsonData, _ := json.Marshal(map[string]interface{}{
-	        "status":  "Error",
-	        "message": dataL,
-	    })
-
-	    fmt.Fprint(response, string(jsonData))
-	    Utils.Logger(2, "Bad Request 'Not Found Router' -> Status Not Found")
-	})
-
+	
 	server := &http.Server{Addr: port, Handler: router}
 
 	sigintChan := make(chan os.Signal, 1)
