b0f73b3066d117ebe841bb9dc57aab65d7a63b38
Qii <57295427+syauqqii@users.noreply.github.com>
2023-05-01 10:29:36 +0700

    Refactor code function ClearScreen()
    Supaya code lebih rapi dan efisien.

--- Nomor 3 - Rest API/Rest-API/Utils/CLS.go
+++ Nomor 3 - Rest API/Rest-API/Utils/CLS.go
@@ -1,25 +1,21 @@
 package Utils
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 	"runtime"
 )
 
 func ClearScreen(){
-	_os := runtime.GOOS
-
-	if _os == "windows" {
+	if runtime.GOOS == "windows" {
 		cmd := exec.Command("cmd", "/c", "cls")
 		cmd.Stdout = os.Stdout
-		cmd.Run()
-
-	} else if _os == "linux" {
+		_ = cmd.Run()
+	} else if runtime.GOOS == "linux" || runtime.GOOS == "darwin" {
 		cmd := exec.Command("clear")
 		cmd.Stdout = os.Stdout
-		cmd.Run()
+		_ = cmd.Run()
   	} else {
-		fmt.Print()
+		return
 	}
 }
